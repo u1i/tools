bacd2df8621b632c3046e469d5d90d385d90654a
Uli <root@localhost>
2017-04-23 16:02:21 +0800

    changed

---
+++ get_github_repos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Get all Github repositories for one user or organization
+# Usage: ./get_github_repos.sh [user]
+# e.g. ./get_github_repos.sh u1i
+
+tmpfile=/tmp/getgitrepos.tmp.$$
+
+if [ "$1" = "" ]
+then
+	echo "Please enter the organization or user name on Github"
+	read username
+else
+	username=$1
+fi
+
+echo Working...
+for d in $(seq 2)
+do
+	wget --quiet -O $tmpfile.$d "https://github.com/$username?page=$d"
+done
+
+cat $tmpfile.* | grep "href=\"\/$username" | sed "s/>.*//;" | tr '"' "\n" | grep $username | sed "s/\/$username//;" | sed "s/^\///;" | sed "s/\/.*//; s/\?.*//;" | sort -u | grep -v "\.atom"
+
+rm $tmpfile.*
